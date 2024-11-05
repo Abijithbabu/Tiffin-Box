d15af546b78b13de666e623c295b9130b37da705
Abhijith Babu <abijithb2002@gmail.com>
2024-11-05 11:26:28 +0530

    init:starter code initialized

---
+++ src/app/(section)/Hero.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+const Hero = ({}) => {
+  return (
+    <section id="hero" className="flex w-full items-center justify-center">
+      <div className="relative flex min-h-[100vh] w-full items-center justify-center">
+        <div className="absolute left-0 top-0 h-full w-full overflow-hidden">
+          <video
+            className="min-h-full min-w-full object-cover"
+            style={{ objectPosition: "30% center" }}
+            src="https://foodo-web-videos.s3.eu-west-2.amazonaws.com/bavette-restaurant.mov"
+            autoPlay
+            loop
+            muted
+            playsInline
+            poster="https://foodo-web-videos.s3.eu-west-2.amazonaws.com/bavette-thumbnail.png"
+          ></video>
+        </div>
+        <div className="z-50 flex h-full w-full max-w-[1300px] items-center justify-center">
+          <div className="flex w-fit flex-col items-end justify-center">
+            <h1 className="font-playfair text-7xl font-normal uppercase leading-[80%] sm:text-8xl md:text-9xl lg:text-[11.62rem]">
+              bavette
+            </h1>
+            <Image
+              src="/images/steak-house.svg"
+              width={243}
+              height={58}
+              alt="steak house"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
